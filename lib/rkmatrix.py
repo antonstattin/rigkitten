f204d8c4da574f33d35be7e7fee02a513a7e91c3
Anton Stattin <astattin@mf1hex_20.milford.hq>
2018-08-06 18:53:39 +0200

    finished a first version of the aim constraint

--- lib/rkmatrix.py
+++ lib/rkmatrix.py
@@ -3,7 +3,24 @@ from . import rkattribute, rkdecorators
 reload(rkattribute)
 reload(rkdecorators)
 
-def getMMatrixFromAttr(mObj, attrStr):
+def vectorBetweenObj(objA, objB):
+    """ Returns a vector between two MObjects
+
+        :param objA: from
+        :type objA: MObject
+
+        :param objA: from
+        :type objA: MObject
+    """
+    objA_WsMtx = getMMatrixFromAttr(objA, "worldMatrix")
+    objB_WsMtx = getMMatrixFromAttr(objB, "worldMatrix")
+
+    objA_point = om.MTransformationMatrix(objA_WsMtx).translation(om.MSpace.kWorld)
+    objB_point = om.MTransformationMatrix(objB_WsMtx).translation(om.MSpace.kWorld)
+
+    return objB_point - objA_point
+
+def getMMatrixFromAttr(mObj, attrStr, inverse=False):
     """ util script to grab matrix from attribute
 
         :param mObj: MObject with matrix attribute
@@ -18,11 +35,16 @@ def getMMatrixFromAttr(mObj, attrStr):
     matrixattr =  mfnDn.attribute(attrStr)
 
     matrixPlug = om.MPlug( mObj, matrixattr )
-    matrixPlug = matrixPlug.elementByLogicalIndex( 0 )
-    matrixMObj = matrixPlug.asMObject()
+    try:
+        matrixPlug = matrixPlug.elementByLogicalIndex( 0 )
+        matrixMObj = matrixPlug.asMObject()
+    except: matrixMObj = matrixPlug.asMObject()
+
+
 
     mfMatrixData = om.MFnMatrixData(matrixMObj)
 
+    if inverse: return mfMatrixData.matrix().inverse()
     return mfMatrixData.matrix()
 
 @rkdecorators.enable_om_undo
@@ -135,12 +157,15 @@ def spaceSwitch(*args, **kwargs):
 def aimConstraint(driver, driven, **kwargs):
     """
         Creates a matrix aim constraint between two objects.
+
         :Keyword Arguments:
             * name | n  - name of setup (str)
             * maintainOffset | mo  - if setup should maintain offset (bool)
             # upVector | up - up vector (default Y)
             # upObject | uobj - up Object
             # aimVector | aim - aim vector (default X)
+            # aimObject | aobj - calc aim-vector from this obj to driver
+                                 instead of driven -> driver. prevents bengin cycle
             * preventBenignCycle | pbc  - prevent benign cycles, grab parent instead of
                                    using inverseParentMatrix from driven.
 
@@ -151,16 +176,16 @@ def aimConstraint(driver, driven, **kwargs):
     mSel.add(driven)
     mSel.add(driver)
 
-    driven = mSel.getDependNode(0)
-    driver = mSel.getDependNode(1)
+    driven_mobj = mSel.getDependNode(0)
+    driver_mobj = mSel.getDependNode(1)
 
     # query kwargs
     name = kwargs.get("name", kwargs.get("n", "matrixConstraint"))
     mo = kwargs.get("maintainOffset", kwargs.get("mo", False))
     aimVector = kwargs.get("aimVector", kwargs.get("aim", "x"))
-    upObj = kwargs.get("upObject", kwargs.get("up", None))
+    upObj = kwargs.get("upObject", kwargs.get("upobj", None))
     pbc = kwargs.get("preventBenignCycle", kwargs.get("pbc", True))
-
+    aimObj = kwargs.get("aimObject", kwargs.get("aobj", None))
 
     # create modifier
     dgMod = om.MDGModifier()
@@ -181,44 +206,53 @@ def aimConstraint(driver, driven, **kwargs):
     crossPrdct = dgMod.createNode("vectorProduct")
     dgMod.renameNode(crossPrdct, "_{0}_CrossPrdct".format(name))
 
+    aimMtxDcmp = dgMod.createNode("decomposeMatrix")
+    dgMod.renameNode(aimMtxDcmp, "_{0}_OutDMtx".format(name))
+
     rkattribute.getPlug(crossPrdct, "operation").setInt(2)
     rkattribute.getPlug(getAimVecPMA, "operation").setInt(2)
 
     # connect pointMatrixMult
-    dgMod.connect(rkattribute.getPlug(driver, "worldMatrix").elementByLogicalIndex(0),
+    dgMod.connect(rkattribute.getPlug(driver_mobj, "worldMatrix").elementByLogicalIndex(0),
                   rkattribute.getPlug(toPoint, "inMatrix"))
 
-    dgMod.connect(rkattribute.getPlug(driver, "rotatePivot"),
+    dgMod.connect(rkattribute.getPlug(driver_mobj, "rotatePivot"),
                   rkattribute.getPlug(toPoint, "inPoint"))
 
-    dgMod.connect(rkattribute.getPlug(driven, "worldMatrix").elementByLogicalIndex(0),
-                  rkattribute.getPlug(fromPoint, "inMatrix"))
+    if aimObj:
+        aimSel = om.MSelectionList()
+        aimSel.add(aimObj)
+        aimObj = aimSel.getDependNode(0)
+    else: aimObj = driven_mobj
 
-    dgMod.connect(rkattribute.getPlug(driven, "rotatePivot"),
-                  rkattribute.getPlug(fromPoint, "inPoint"))
+    dgMod.connect(rkattribute.getPlug(aimObj, "worldMatrix").elementByLogicalIndex(0),
+                      rkattribute.getPlug(fromPoint, "inMatrix"))
+
+    dgMod.connect(rkattribute.getPlug(aimObj, "rotatePivot"),
+                      rkattribute.getPlug(fromPoint, "inPoint"))
 
     if "-" in aimVector:
         dgMod.connect(rkattribute.getPlug(fromPoint, "output"),
-                      rkattribute.getPlug(getAimVecPMA, "input3D").elementByLogicalIndex(1))
+                      rkattribute.getPlug(getAimVecPMA, "input3D").elementByLogicalIndex(0))
 
         dgMod.connect(rkattribute.getPlug(toPoint, "output"),
-                      rkattribute.getPlug(getAimVecPMA, "input3D").elementByLogicalIndex(0))
+                      rkattribute.getPlug(getAimVecPMA, "input3D").elementByLogicalIndex(1))
     else:
         dgMod.connect(rkattribute.getPlug(fromPoint, "output"),
-                      rkattribute.getPlug(getAimVecPMA, "input3D").elementByLogicalIndex(0))
+                      rkattribute.getPlug(getAimVecPMA, "input3D").elementByLogicalIndex(1))
 
         dgMod.connect(rkattribute.getPlug(toPoint, "output"),
-                      rkattribute.getPlug(getAimVecPMA, "input3D").elementByLogicalIndex(1))
+                      rkattribute.getPlug(getAimVecPMA, "input3D").elementByLogicalIndex(0))
 
     if upObj:
 
         # Create a normalized vector
-        mSel.add(upObj)
-        upObj = mSel.getDependNode(2)
-        upObjMfnT = om.MFnTransform(upObj)
+        if upObj != driver:
+            mSel.add(upObj)
+            upObj = mSel.getDependNode(2)
+        else: upObj = driver_mobj
 
-        vec = upObjMfnT.translation(om.MSpace.kObject)
-        offsetMtx = om.MMatrix((1,0,0,0, 0,1,0,0, 0,0,1,0, vec.x,vec.y+1,vec.z, 0))
+        offsetMtx = om.MMatrix((1,0,0,0, 0,1,0,0, 0,0,1,0, 0, 1, 0, 0))
 
         upVecMtxMult = dgMod.createNode("multMatrix")
         dgMod.renameNode(upVecMtxMult, "_{0}_UpMtxMult".format(name))
@@ -227,13 +261,122 @@ def aimConstraint(driver, driven, **kwargs):
         upVecMtxDcmp = dgMod.createNode("decomposeMatrix")
         dgMod.renameNode(upVecMtxDcmp, "_{0}UpVector_DMtx".format(name))
 
-        mtxPrdct = om.MFnMatrixData().create(offsetMtx)
+        invMtx = getMMatrixFromAttr(upObj, "worldInverseMatrix")
+        mtxPrdct = om.MFnMatrixData().create(offsetMtx*invMtx)
+
 
         rkattribute.getPlug(upVecMtxMult, "matrixIn").elementByLogicalIndex(0).setMObject(mtxPrdct)
         dgMod.connect(rkattribute.getPlug(upObj, "worldMatrix").elementByLogicalIndex(0),
                       rkattribute.getPlug(upVecMtxMult, "matrixIn").elementByLogicalIndex(1))
 
+        dgMod.connect(rkattribute.getPlug(upVecMtxMult, "matrixSum"),
+                      rkattribute.getPlug(upVecMtxDcmp, "inputMatrix"))
 
+        dgMod.connect(rkattribute.getPlug(upVecMtxDcmp, "outputTranslateX"),
+                      rkattribute.getPlug(fbfm, "in10"))
+
+        dgMod.connect(rkattribute.getPlug(upVecMtxDcmp, "outputTranslateY"),
+                      rkattribute.getPlug(fbfm, "in11"))
+
+        dgMod.connect(rkattribute.getPlug(upVecMtxDcmp, "outputTranslateZ"),
+                      rkattribute.getPlug(fbfm, "in12"))
+
+        dgMod.connect(rkattribute.getPlug(upVecMtxDcmp, "outputTranslate"),
+                      rkattribute.getPlug(crossPrdct, "input1"))
+
+    else:
+        rkattribute.getPlug(fbfm, "in11").setFloat(1)
+        rkattribute.getPlug(upVecMtxDcmp, "input1Y").setFloat(1)
+
+
+    # create aim matrix
+    dgMod.connect(rkattribute.getPlug(getAimVecPMA, "output3Dx"),
+                  rkattribute.getPlug(fbfm, "in00"))
+
+    dgMod.connect(rkattribute.getPlug(getAimVecPMA, "output3Dy"),
+                  rkattribute.getPlug(fbfm, "in01"))
+
+    dgMod.connect(rkattribute.getPlug(getAimVecPMA, "output3Dz"),
+                  rkattribute.getPlug(fbfm, "in02"))
+
+    dgMod.connect(rkattribute.getPlug(getAimVecPMA, "output3D"),
+                  rkattribute.getPlug(crossPrdct, "input2"))
+
+    dgMod.connect(rkattribute.getPlug(crossPrdct, "outputX"),
+                  rkattribute.getPlug(fbfm, "in20"))
+
+    dgMod.connect(rkattribute.getPlug(crossPrdct, "outputY"),
+                  rkattribute.getPlug(fbfm, "in21"))
+
+    dgMod.connect(rkattribute.getPlug(crossPrdct, "outputZ"),
+                  rkattribute.getPlug(fbfm, "in22"))
+
+
+    if mo:
+        mult = dgMod.createNode("multMatrix")
+        dgMod.renameNode(mult, "_{0}{1}_MO".format(name, driven.title()))
+
+        inA = rkattribute.getPlug(mult, "matrixIn").elementByLogicalIndex(0)
+        inB = rkattribute.getPlug(mult, "matrixIn").elementByLogicalIndex(1)
+        fbfout = rkattribute.getPlug(fbfm, "output")
+
+        # create the offset (need to create the aim matrix)
+        dMtx = getMMatrixFromAttr(driven_mobj, "worldMatrix")
+        aimVector = vectorBetweenObj(driven_mobj, driver_mobj)
+        upVector = om.MVector(0,1,0)
+
+        aimCrsUp = upVector ^ aimVector
+        aimMtx = om.MMatrix((aimVector.x, aimVector.y, aimVector.z, 0,
+                             upVector.x, upVector.y, upVector.z, 0,
+                             aimCrsUp.x, aimCrsUp.y, aimCrsUp.z,
+                             0, 0, 0, 0, 0))
+
+        mtxPrdct = om.MFnMatrixData().create(dMtx * aimMtx.inverse())
+        inA.setMObject(mtxPrdct)
+        dgMod.connect(fbfout, inB)
+
+        outPlug = rkattribute.getPlug(mult, "matrixSum")
+        #dgMod.connect(rkattribute.getPlug(mult, "matrixSum"),
+                      #rkattribute.getPlug(aimMtxDcmp, "inputMatrix"))
+    else:
+        outPlug = rkattribute.getPlug(fbfm, "output")
+        #dgMod.connect(rkattribute.getPlug(fbfm, "output"),
+                      #rkattribute.getPlug(aimMtxDcmp, "inputMatrix"))
+
+
+    if pbc:
+        parentMObj = om.MFnDagNode(driven_mobj).parent(0)
+        parent = om.MFnDependencyNode(parentMObj)
+
+        # if we no parent skip step else get parent and connect worldmtx
+        if parent.name() != "world":
+            invPMtx = dgMod.createNode("multMatrix")
+            dgMod.renameNode(upVecMtxMult, "_{0}_InvParMtx".format(name))
+
+
+            dgMod.connect(outPlug, rkattribute.getPlug(invPMtx, "matrixIn").elementByLogicalIndex(0))
+
+            dgMod.connect(rkattribute.getPlug(parentMObj, "worldInverseMatrix").elementByLogicalIndex(0),
+                          rkattribute.getPlug(invPMtx, "matrixIn").elementByLogicalIndex(1))
+
+            dgMod.connect(rkattribute.getPlug(invPMtx, "matrixSum"),
+                          rkattribute.getPlug(aimMtxDcmp, "inputMatrix"))
+        else:
+            dgMod.connect(outPlug, rkattribute.getPlug(aimMtxDcmp, "inputMatrix"))
+    else:
+        invPMtx = dgMod.createNode("multMatrix")
+        dgMod.renameNode(upVecMtxMult, "_{0}_InvParMtx".format(name))
+
+        dgMod.connect(outPlug, rkattribute.getPlug(invParMult, "matrixIn").elementByLogicalIndex(0))
+        dgMod.connect(rkattribute.getPlug(driven_mobj, "parentInverseMatrix").elementByLogicalIndex(0),
+                      rkattribute.getPlug(invParMult, "matrixIn").elementByLogicalIndex(1))
+
+
+        dgMod.connect(rkattribute.getPlug(invPMtx, "matrixSum"),
+                      rkattribute.getPlug(aimMtxDcmp, "inputMatrix"))
+
+    dgMod.connect(rkattribute.getPlug(aimMtxDcmp, "outputRotate"),
+                  rkattribute.getPlug(driven_mobj, "rotate"))
 
     dgMod.doIt()
 
